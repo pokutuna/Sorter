9c005926e69fda45c9c268bbce52fb583512b3c1
pokutuna <popopopopokutuna@gmail.com>
2011-08-15 23:23:02 +0900

    Sorter実装 & テスト通った

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,48 @@
-# implement this
+package Sorter;
+
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless($self, $class);
+    $self->_init;
+    return $self;
+}
+
+sub _init {
+    my $self = shift;
+    $self->{values} = [];
+}
+
+sub set_values {
+    my $self = shift;
+    $self->{values} = \@_;
+}
+
+sub get_values {
+    my $self = shift;
+    @{$self->{values}};
+}
+
+sub sort {
+    my $self = shift;
+    my @sorted = &_qsort($self->get_values);
+    $self->set_values(@sorted);
+}
+
+sub _qsort {
+    return () if @_ < 1;
+
+    my ($pivot, @values) = @_;
+    my (@left, @right) = ((), ());
+    foreach my $v (@values) {
+        ($v < $pivot) ? push(@left, $v) : push(@right, $v);
+    }
+
+    return (&_qsort(@left), $pivot, &_qsort(@right));
+}
+
+1;
+
